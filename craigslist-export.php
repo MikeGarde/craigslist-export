316f95703b485b4e8049f7c3ba292b91b14943e8
Jeff Clayton <jclayton2379@gmail.com>
2013-05-04 15:22:33 -0400

    Setting up menu

--- craigslist-export.php
+++ craigslist-export.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: craigslist-export
+Plugin Name: craigslist export
 Plugin URI: https://github.com/MikeGarde/craigslist-export
 Description: Export post-type to craigslist
 Author: Jeff & Mike
@@ -8,3 +8,21 @@ Text Domain: cle
 Version: 0.1
 */
 
+
+
+class craigslist_export_settings {
+	function __construct() {
+		add_action('admin_menu', array(&$this, 'admin_menu'));
+	}
+	function admin_menu () {
+		add_options_page('craigslist export settings',
+						'craigslist export',
+						'manage_options',
+						'craigslist_export_settings',
+						array($this, 'settings_page'));
+	}
+	function  settings_page () {
+		require_once('view/settings.php');
+	}
+}
+new craigslist_export_settings;
---
+++ view/settings.php
@@ -0,0 +1,1 @@
+<h2>craigslist export settings</h2>
