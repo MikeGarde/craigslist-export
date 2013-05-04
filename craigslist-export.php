0870dcb36aa85559c15646e40edeb08e61ca6ddf
Jeff Clayton <jclayton2379@gmail.com>
2013-05-04 14:56:23 -0400

    Getting Started

---
+++ craigslist-export.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: craigslist-export
+Plugin URI: https://github.com/MikeGarde/craigslist-export
+Description: Export post-type to craigslist
+Author: Jeff & Mike
+Text Domain: cle
+Version: 0.1
+*/
+
